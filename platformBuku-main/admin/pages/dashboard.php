18019e313357f0b41efe916b15e618f0bdc7b33f
adhanirp <89771119+adhanir@users.noreply.github.com>
2022-06-06 09:39:17 +0700

    Add files via upload

---
+++ platformBuku-main/admin/pages/dashboard.php
@@ -0,0 +1,49 @@
+<!-- Main content -->
+<div class="content pt-5">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-4 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>24</h3>
+                        <p>Transaksi Baru</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-bag"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">Kelola <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-orange">
+                    <div class="inner">
+                        <h3>12</h3>
+                        <p>Permintaan Kode Akses</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-key"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">Kelola <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>16</h3>
+                        <p>Pengguna Baru</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-add"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">Kelola <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</div>
+<!-- /.content -->
